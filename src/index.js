87d004b9c1ca23ed4815e7333100fb00001990db
chayan19 <choudhury.chayan@gmail.com>
2021-07-02 12:55:15 +0530

    12.55 pm

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`ecomapp: listening on port ${port}`);
+});
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -15,3 +23,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
